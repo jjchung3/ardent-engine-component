432af351b7634d212af606b790a101554abb97d7
jjchung3 <joejoonchung@gmail.com>
2020-10-31 04:09:53 -0700

    third

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg'
+import './App.css'
+import ScriptView from './ScriptView'
+
+function App() {
+  return (
+    <div className="App">
+      <ScriptView />
+    </div>
+  )
+}
+
+export default App
---
+++ src/ScriptView.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect, Fragment } from 'react'
+
+const ScriptView = props => {
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ScriptView
